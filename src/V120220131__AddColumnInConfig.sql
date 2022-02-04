8ef9cdf0288e5d9789b1cafd96b13ae948edbef4
sajanmje27 <sajanmje82@gmail.com>
2022-02-04 17:15:30 -0500

    added token service implementation

--- src/V120220131__AddColumnInConfig.sql
+++ src/V120220131__AddColumnInConfig.sql
@@ -1,2 +1,8 @@
 ALTER TABLE IF EXISTS public.config_key_value
     ADD COLUMN is_application_variable boolean;
+
+ALTER TABLE IF EXISTS public.actionoption
+    ADD COLUMN condition_type character varying;
+
+ALTER TABLE IF EXISTS public.action
+    ADD COLUMN isoptional boolean;
